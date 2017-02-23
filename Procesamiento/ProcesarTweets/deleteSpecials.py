0a0a91235f540483f12e18ac98b7688280d9ad3b
Felip7 <esteban.diaz@epn.edu.ec>
2017-02-22 23:56:00 -0500

    Update deleteSpecials.py
    Añadir descripción

--- Procesamiento/ProcesarTweets/deleteSpecials.py
+++ Procesamiento/ProcesarTweets/deleteSpecials.py
@@ -1,25 +1,27 @@
+#Código escrito en lenguaje python que permite leer el archivo .txt creado por openFile.py
+#con el objetivo de limpiar el texto de cada tweet recolectado.
+
 import re, string
 
-
+#Función que elimina todos los URL que contienen los tweets a través de una expresión regular
 def eliminarURL(text):
     link_regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
     links = re.findall(link_regex, text)
     for link in links:
         text = text.replace(link[0], ', ')
-
     return text
 
-
+#Función que elimina todos las combinaciones que generan Emoticones 
+#en los tweets a través de una expresión regular
 def eliminarCarita(text):
     link_regex = re.compile(
         '(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)')
     links = re.findall(link_regex, text)
     for link in links:
         text = text.replace(link[0], ', ')
-
     return text
 
-
+#Función que elimina todos los hashtag (#) que contienen los tweets 
 def eliminarHashtag(text):
     outputSentence = ""
     changes = 0;
@@ -32,8 +34,9 @@ def eliminarHashtag(text):
             outputSentence += letter
             returnMsg = str(changes)
     return outputSentence
-
-with open('/root/PycharmProjects/ProcesarTweets/tweetsValue.txt') as f:
+#Ejemplo de directorio Proyecto Pycharm:
+#with open('/root/PycharmProjects/ProcesarTweets/tweetsValue.txt') as f:
+with open('') as f:
     for t in f:
         #eliminarURL(t)
         res1 = eliminarURL(t)
@@ -43,7 +46,8 @@ with open('/root/PycharmProjects/ProcesarTweets/tweetsValue.txt') as f:
 
         #eliminarHashtag(res2)
         res3 = eliminarHashtag(res2)
-
-        f2=open('/root/PycharmProjects/ProcesarTweets/processedTweets.txt','a')
+        #Ejemplo de directorio Proyecto Pycharm:
+        #f2=open('/root/PycharmProjects/ProcesarTweets/processedTweets.txt','a')
+        f2=open('','a')
         f2.write(res3)
         f2.close()
